e5430e0e25a890c070cb54816d17af5b275e44af
nikos <nikosnellman@hotmail.com>
2020-06-25 21:00:24 +0300

    Program get
    Ohjelmat voi hakea tietokannasta get pyynnöllä.

--- src/routers/program.js
+++ src/routers/program.js
@@ -1,15 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const Program = require("../models/program");
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
 
 router.get("/", (req, res, next) => {
-  res.send({ foo: "bar" });
+  Program.find({})
+    .then((programs) => {
+      res.send(programs);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
 
 router.post("/", (req, res, next) => {
   console.log(req.body);
 
-  res.send({ terve: "hello" });
+  const program = new Program(req.body);
+
+  program
+    .save()
+    .then(() => {
+      res.status(201).send(program);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send(error);
+    });
 });
 
 module.exports = router;
